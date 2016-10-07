db30bf21312b2d3cd69788c51c34493122fce747
TheProxy <theproxyjomgegar@gmail.com>
2016-10-07 15:11:40 +0200

    Copyright & Description Uppdate

--- de4dot.code/deobfuscators/NetPack/ResourceDecrypter.cs
+++ de4dot.code/deobfuscators/NetPack/ResourceDecrypter.cs
@@ -1,6 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+    Copyright (C) 2016 TheProxy
+
+    This file is part of modified de4dot.
+
+    de4dot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    de4dot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with de4dot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 using System.IO;
